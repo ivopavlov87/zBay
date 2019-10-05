9888157d9a5ded48e61f3343d58a1a724b636bd7
Jonathan <jonathanjohnson023@gmail.com>
2019-10-04 19:21:29 -1000

    Got some CSS done basic layout done

--- client/src/components/homes/HomeIndex.js
+++ client/src/components/homes/HomeIndex.js
@@ -2,9 +2,12 @@ import React from "react";
 // import React, { Component } from "react";
 // import gql from "graphql-tag";
 import { Query } from "react-apollo";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+import Map from "../map/map_view"
 
 import Queries from "../../graphql/queries";
+import "./home_index.css"
 const { FETCH_HOMES } = Queries;
 
 const HomeIndex = () => {
@@ -15,13 +18,25 @@ const HomeIndex = () => {
         if (error) return `Error! ${error.message}`;
 
         return (
-          <ul>
-            {data.homes.map(home => (
-              <Link key={home._id} to={`/homes/${home._id}`}>
-                <li>{home.name}</li>
-              </ Link>
-            ))}
-          </ul>
+          <div className="home-index">
+            <Map />
+            <ul className="homes-ul">
+              {data.homes.map(home => (
+                <Link key={home._id} to={`/homes/${home._id}`}>
+                  <li>
+                    <div className="top-info">
+                      {/* house.photo it will be a backround*/}
+                      {/* button that addToWatchList this will probably be a function  */}
+                      {home.name}
+                    </div>
+                    <div className="bottom-info">
+                      {/* house.info you know info*/}
+                    </div>
+                  </li>
+                </ Link>
+              ))}
+            </ul>
+          </div>
         );
       }}
     </Query>
--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,8 @@ import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "react-apollo";
 import { onError } from "apollo-link-error";
 import { ApolloLink } from "apollo-link";
-
 import Mutations from "./graphql/mutations"
+
 const { VERIFY_USER } = Mutations
 
 const token = localStorage.getItem("auth-token");
