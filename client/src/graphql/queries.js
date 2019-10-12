8f0303ee80837fb0168505c1d40dc6bd790734ab
Jonathan <jonathanjohnson023@gmail.com>
2019-10-12 09:30:14 -1000

    added commas

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -179,36 +179,36 @@ export default {
   `,
   ADVANCED_SEARCH: gql`
     query AdvancedSearch(
-      $nameQuery: String
-      $categoryQuery: String
-      $descriptionQuery: String
-      $streetAddressQuery: String
-      $cityQuery: String
-      $stateQuery: String
-      $yearBuiltQuery: Int
-      $sqftQuery: Int
-      $zipcodeQuery: Int
-      $storiesQuery: Int
-      $bedroomsQuery: Int
-      $bathroomsQuery: Int
-      $garageQuery: Boolean
-      $basementQuery: Boolean
+      $nameQuery: String,
+      $categoryQuery: String,
+      $descriptionQuery: String,
+      $streetAddressQuery: String,
+      $cityQuery: String,
+      $stateQuery: String,
+      $yearBuiltQuery: Int,
+      $sqftQuery: Int,
+      $zipcodeQuery: Int,
+      $storiesQuery: Int,
+      $bedroomsQuery: Int,
+      $bathroomsQuery: Int,
+      $garageQuery: Boolean,
+      $basementQuery: Boolean,
     ) {
       advancedSearch(
-        nameQuery: $nameQuery
-        categoryQuery: $categoryQuery
-        descriptionQuery: $descriptionQuery
-        streetAddressQuery: $streetAddressQuery
-        cityQuery: $cityQuery
-        stateQuery: $stateQuery
-        yearBuiltQuery: $yearBuiltQuery
-        sqftQuery: $sqftQuery
-        zipcodeQuery: $zipcodeQuery
-        storiesQuery: $storiesQuery
-        bedroomsQuery: $bedroomsQuery
-        bathroomsQuery: $bathroomsQuery
-        garageQuery: $garageQuery
-        basementQuery: $basementQuery
+        nameQuery: $nameQuery,
+        categoryQuery: $categoryQuery,
+        descriptionQuery: $descriptionQuery,
+        streetAddressQuery: $streetAddressQuery,
+        cityQuery: $cityQuery,
+        stateQuery: $stateQuery,
+        yearBuiltQuery: $yearBuiltQuery,
+        sqftQuery: $sqftQuery,
+        zipcodeQuery: $zipcodeQuery,
+        storiesQuery: $storiesQuery,
+        bedroomsQuery: $bedroomsQuery,
+        bathroomsQuery: $bathroomsQuery,
+        garageQuery: $garageQuery,
+        basementQuery: $basementQuery,
       ) {
         _id
         name
