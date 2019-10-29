92d0b68bcb7c1a1bcc8f504781d6ba9371b33d72
ivopavlov <pavlov.ivo@gmail.com>
2019-10-28 20:55:55 -0700

    error handling adjustment

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -28,7 +28,8 @@ class Login extends Component {
 
   render() {
 
-    const errors = this.state.errors ? (
+    // possibly need to remove/edit " && this.state.errors.graphQLErrors"
+    const errors = (this.state.errors && this.state.errors.graphQLErrors) ? (
       <li className="modal-li-errors">
         {this.state.errors.graphQLErrors[0].message}
       </li>
