5ad4de0b3e7d52ba9ec6052ad97ae42165515158
anisurrkhan <anisurk24@gmail.com>
2019-11-06 23:23:13 -0800

    more css changes

--- client/src/components/search/SearchBar.jsx
+++ client/src/components/search/SearchBar.jsx
@@ -20,11 +20,11 @@ class SearchBar extends React.Component{
         return (
            <div className="searchbar-container">
                <Search />
-                <button value="sale-type" onClick={(e) => this.handleOpen(e, "sale-type")} className="searchbar-button">Sale type</button>
+                {/* <button value="sale-type" onClick={(e) => this.handleOpen(e, "sale-type")} className="searchbar-button">Sale type</button>
                 <button value="price" onClick={(e) => this.handleOpen(e, "price")} className="searchbar-button">Price</button>
                 <button value="beds" onClick={(e) => this.handleOpen(e, "beds")} className="searchbar-button">Beds</button>
                 <button value="home-type" onClick={(e) => this.handleOpen(e, "home-type")} className="searchbar-button">Home type</button>
-                <button value="more" onClick={(e) => this.handleOpen(e, "more")} className="searchbar-button">More</button>
+                <button value="more" onClick={(e) => this.handleOpen(e, "more")} className="searchbar-button">More</button> */}
            </div>
         )
     }
