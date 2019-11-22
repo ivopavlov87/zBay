da9882ed606641a45b390c61b5a5d61e7c98b7d8
anisurrkhan <anisurk24@gmail.com>
2019-11-21 21:04:18 -0800

    zbay completed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -22,7 +22,7 @@ class Login extends Component {
   updateCache(client, { data }) {
     // debugger;
     client.writeData({
-      data: { isLoggedIn: data.login.loggedIn, _id: data.login._id }
+      data: { isLoggedIn: data.login.loggedIn, _id: data.login._id, modalOpen: data.login.loggedIn ? false : true }
     });
   }
 
